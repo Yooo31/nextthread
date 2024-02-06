9382beb501ef841b567394d2ac8b3f810f132e89
Yooo31 <mrts.yoan@gmail.com>
2024-02-06 23:08:42 +0100

    Components install

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      Hello world !
+    <div className="m-4">
+      <Input></Input>
+      <Button>Click me !</Button>
     </div>
   );
 }
