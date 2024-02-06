dcaf0e669fa8064a2fc5b08b78a15da88266e469
Yooo31 <mrts.yoan@gmail.com>
2024-02-06 23:49:16 +0100

    Add header

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/src/theme/ThemeProvider";
 import clsx from "clsx";
+import { Header } from "@/src/feature/layout/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,9 +22,10 @@ export default function RootLayout({
       <body className={clsx(inter.className, 'bg-background h-full')}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <div className="flex flex-col h-full">
+            <Header />
             <div className="flex-1 max-w-lg m-auto py-12 w-full">
               {children}
-            </div>
+            H</div>
           </div>
         </ThemeProvider>
       </body>
