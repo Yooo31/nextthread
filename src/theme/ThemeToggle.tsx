f9e01ca6d49c06109bdc840f7ce7e34af4454103
Yooo31 <mrts.yoan@gmail.com>
2024-02-07 00:03:55 +0100

    Add absolute on button to replace the precedent

--- src/theme/ThemeToggle.tsx
+++ src/theme/ThemeToggle.tsx
@@ -17,7 +17,7 @@ export const ThemeToggle = () => {
       }}
     >
       <SunMedium size={20} className='rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0'/>
-      <Moon size={20} className='rotate-90 scale-0 transition-all dark:-rotate-0 dark:scale-100'/>
+      <Moon size={20} className='absolute rotate-90 scale-0 transition-all dark:-rotate-0 dark:scale-100'/>
       <span className="sr-only">Toggle Theme</span>
     </Button>
   );
