edb382c653baa526d07720e11a44baa53279d5e9
Yooo31 <mrts.yoan@gmail.com>
2024-02-14 12:08:37 +0100

    Add id on session

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { getAuthSession } from "@/lib/auth";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getAuthSession();
+
   return (
-    <div className="m-4">
+    <div className="">
+      <p>
+        {JSON.stringify(session, null, 2)}
+      </p>
       <Input></Input>
       <Button>Click me !</Button>
     </div>
--- src/feature/layout/Header.tsx
+++ src/feature/layout/Header.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 export const Header = async () => {
   return (
-    <header className='border-b border-b-accent'>
+    <header className='border-b border-b-accent fixed top-0 bg-backgroun w-full'>
       <div className="container flex items-center py-2 max-w-lg m-auto gap-1">
         <h2 className="text-2xl font-bold mr-auto">NextThread</h2>
         <LoginButton />
